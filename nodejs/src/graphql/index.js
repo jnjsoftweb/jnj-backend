a64748845124627f1529d8b6810e17a7a487ddbc
JnJ Web <jnjsoft.web@gmail.com>
2024-11-20 00:05:05 +0000

    youtube graphql

--- nodejs/src/graphql/index.js
+++ nodejs/src/graphql/index.js
@@ -1,5 +1,6 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import { ApolloServerPluginLandingPageGraphQLPlayground } from '@apollo/server-plugin-landing-page-graphql-playground'; // Playground 플러그인 임포트
 import { typeDefs } from './typeDefs.js';
 import { resolvers } from './youtube/resolvers.js';
 import { GRAPHQL_PORT } from '../env.js';
@@ -8,7 +9,6 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
   formatError: (error) => {
-    // 에러 상세 정보 로깅
     console.error('\n=== GraphQL Error ===');
     console.error('Message:', error.message);
     console.error('Path:', error.path);
@@ -18,6 +18,15 @@ const server = new ApolloServer({
     console.error('=== End Error ===\n');
     return error;
   },
+  plugins: [
+    ApolloServerPluginLandingPageGraphQLPlayground({
+      settings: {
+        'editor.theme': 'dark', // 기본 테마
+        'request.credentials': 'include', // credentials 설정
+      },
+    }),
+  ],
+  introspection: true, // Introspection 활성화
 });
 
 const { url } = await startStandaloneServer(server, {
@@ -25,12 +34,12 @@ const { url } = await startStandaloneServer(server, {
   cors: {
     origin: '*',
     credentials: true,
-    methods: ['POST', 'GET', 'OPTIONS'],
-    allowedHeaders: [
-      'Content-Type',
-      'x-apollo-operation-name',
-      'apollo-require-preflight',
-    ],
+    // methods: ['POST', 'GET', 'OPTIONS'],
+    // allowedHeaders: [
+    //   'Content-Type',
+    //   'x-apollo-operation-name',
+    //   'apollo-require-preflight',
+    // ],
   },
 });
 
